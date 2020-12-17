7aba6e0fce7ce1b143ec48430652855d1ff4aa03
Nick White <nickwhite@MacBook-Pro.local>
2020-12-17 16:39:53 -0600

    fixes deploy script page

--- src/pages/intgs.js
+++ src/pages/intgs.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Travelers } from '../components/data'
-import {server} from '../../../server'
+// import {server} from '../../../server'
 function Intgs() {
     const scriptWriter = Travelers.map(d => {
         const storymapUrl = 'https://uploads.knightlab.com/storymapjs/f16f101e1b9e4f19898b53b294e8d1dd/' + d.storymapurl.substring(d.storymapurl.indexOf('8d1dd') + 6, d.storymapurl.indexOf('index.html')) + 'published.json'
         const wgetLine = 'wget -O '  + d.id + '.json ' + storymapUrl
-        const scpLine = 'scp ' + d.id + '.json  ' + server + 'newberry@web609.webfaction.com:webapps/novareperta/storymaps/' + d.id + '/storymaps/'
+        const scpLine = 'cp ' + d.id + '.json  ' + '/var/www/nova-reperta/static/'
+        const scpLine = 'cp storymaps/' + d.id + '.html  ' + '/var/www/nova-reperta/static/'
         const returnLines = <div><br />{wgetLine}<br />{scpLine}</div>
         return returnLines
     })
